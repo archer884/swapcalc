83276987f2d5116a7736259352f14e23dd125ac0
J/A <archer884@gmail.com>
2018-02-27 15:23:35 -0600

    Add benchmark

---
+++ benches/parse_datetime.rs
@@ -0,0 +1,58 @@
+#![feature(test)]
+
+extern crate chrono;
+extern crate test;
+
+use chrono::format::*;
+use chrono::{NaiveDateTime, TimeZone, Utc};
+use test::Bencher;
+
+static DATE: &str = "2018-02-27 16:32:13.802940 UTC";
+
+#[bench]
+fn format_string(b: &mut Bencher) {
+    let format = "%F %T%.f UTC";
+
+    b.iter(|| {
+        let datetime = NaiveDateTime::parse_from_str(DATE, format)
+            .map(|datetime| Utc.from_local_datetime(&datetime).single().unwrap());
+
+        test::black_box(datetime.unwrap());
+    });
+}
+
+#[bench]
+fn item_iterator(b: &mut Bencher) {
+    
+    #[derive(Default)]
+    struct ParseItems(u8);
+
+    impl Iterator for ParseItems {
+        type Item = Item<'static>;
+
+        fn next(&mut self) -> Option<Self::Item> {
+            self.0 += 1;
+            match self.0 {
+                1 => Some(Item::Numeric(Numeric::Year, Pad::Zero)),
+                3 => Some(Item::Numeric(Numeric::Month, Pad::Zero)),
+                5 => Some(Item::Numeric(Numeric::Day, Pad::Zero)),
+                6 => Some(Item::Space(" ")),
+                7 => Some(Item::Numeric(Numeric::Hour, Pad::Zero)),
+                9 => Some(Item::Numeric(Numeric::Minute, Pad::Zero)),
+                11 => Some(Item::Numeric(Numeric::Second, Pad::Zero)),
+                12 => Some(Item::Fixed(Fixed::Nanosecond6)),
+                13 => Some(Item::Literal(" UTC")),
+                2 | 4 => Some(Item::Literal("-")),
+                8 | 10 => Some(Item::Literal(":")),
+
+                _ => None,
+            }
+        }
+    }
+
+    b.iter(|| {
+        let mut parsed = Parsed::default();
+        parse(&mut parsed, DATE, ParseItems::default()).unwrap();
+        test::black_box(parsed.to_datetime_with_timezone(&Utc).unwrap());
+    });
+}
