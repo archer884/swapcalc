6c553966f88f4557b741e9c2107646ca4e261ba7
J/A <archer884@gmail.com>
2018-02-27 15:28:10 -0600

    Add comment on performance.

--- benches/parse_datetime.rs
+++ benches/parse_datetime.rs
@@ -21,9 +21,30 @@ fn format_string(b: &mut Bencher) {
     });
 }
 
+// Fun fact: I have tried two iterator implementations here, and one is significantly faster.
+//
+// In spite of my best guess, the following match expression contents are slower than the one in 
+// use below:
+//
+// 1 => Some(Item::Numeric(Numeric::Year, Pad::Zero)),
+// 2 => Some(Item::Literal("-")),
+// 3 => Some(Item::Numeric(Numeric::Month, Pad::Zero)),
+// 4 => Some(Item::Literal("-")),
+// 5 => Some(Item::Numeric(Numeric::Day, Pad::Zero)),
+// 6 => Some(Item::Space(" ")),
+// 7 => Some(Item::Numeric(Numeric::Hour, Pad::Zero)),
+// 8 => Some(Item::Literal(":")),
+// 9 => Some(Item::Numeric(Numeric::Minute, Pad::Zero)),
+// 10 => Some(Item::Literal(":")),
+// 11 => Some(Item::Numeric(Numeric::Second, Pad::Zero)),
+// 12 => Some(Item::Fixed(Fixed::Nanosecond6)),
+// 13 => Some(Item::Literal(" UTC")),
+//
+// I would have expected this to be faster as a result of just going in order, etc.
+
 #[bench]
 fn item_iterator(b: &mut Bencher) {
-    
+
     #[derive(Default)]
     struct ParseItems(u8);
 
